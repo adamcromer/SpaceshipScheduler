2d8c084f90fd8c8f656e3c9db6f299350ff154bc
adamjorycromer <adamjorycromer@gmail.com>
2018-12-13 17:07:31 -0700

    submit works and uploads to firebase

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,59 @@
 //n.toLocaleString()
 $(document).ready(function () {
 
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDwYjQG2MkZ_sQVxXc4rkhBbyDO8DXUZes",
+        authDomain: "spaceshipscheduler.firebaseapp.com",
+        databaseURL: "https://spaceshipscheduler.firebaseio.com",
+        projectId: "spaceshipscheduler",
+        storageBucket: "spaceshipscheduler.appspot.com",
+        messagingSenderId: "750832104755"
+    };
+
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
     submit = $("#submit");
 
     submit.click(function (event) {
         event.preventDefault();
 
+        //Grabs the input variables values
+        var name = $("#name").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTime = $("#firstTime").val().trim();
+        var frequency = $("#frequency").val().trim();
+
+        //Resets the values
+        $("#name").val("");
+        $("#destination").val("");
+        $("#firstTime").val("");
+        $("#frequency").val("");
+
+        console.log(name);
+        console.log(destination);
+        console.log(firstTime);
+        console.log(frequency);
+
+
+        database.ref().push({
+
+            name: name,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency
+        });
+
+
+
     });
+
+
+
+
+
+
 
 });
 
