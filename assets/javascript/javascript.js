ff520d7e5739c437010d8910ff8f125172213f11
adamjorycromer <adamjorycromer@gmail.com>
2018-12-17 17:10:13 -0700

    cleaned up code and fixed moment function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
     submit = $("#submit");
     table = $("#tableBody");
 
-
     submit.click(function (event) {
         event.preventDefault();
 
@@ -26,56 +25,46 @@ $(document).ready(function () {
         var firstTime = $("#firstTime").val().trim();
         var frequency = $("#frequency").val().trim();
 
-        //Resets the values
-        $("#name").val("");
-        $("#destination").val("");
-        $("#firstTime").val("");
-        $("#frequency").val("");
+        if (name === "" || destination === "" || firstTime === "" || frequency === "") {
 
-        database.ref().push({
+            alert("Please fill in all details to add new train");
+        }
 
-            name: name,
-            destination: destination,
-            firstTime: firstTime,
-            frequency: frequency
-        });
+        else {
+            //Resets the values
+            $("#name").val("");
+            $("#destination").val("");
+            $("#firstTime").val("");
+            $("#frequency").val("");
+
+            //Adds the inputs to Firebase
+            database.ref().push({
+                name: name,
+                destination: destination,
+                firstTime: firstTime,
+                frequency: frequency
+            });
+        }
     });
 
+    //Pulls info from Firebase.
     database.ref().on("child_added", function (snapshot) {
 
         var newRow = $("<tr class='pFont black'>");
         var shipName = $("<td>").text(snapshot.val().name);
         var shipDest = $("<td>").text(snapshot.val().destination);
         var shipFreq = $("<td>").text("Every " + (snapshot.val().frequency) + " Minutes");
-        var frequency = snapshot.val().frequency;
 
-        var firstTime = snapshot.val().firstTime;
+        var frequency = parseInt(snapshot.val().frequency);
+        var firstTime = parseInt(snapshot.val().firstTime);
         var currentTime = moment();
-        console.log(firstTime);
-        console.log(currentTime);
 
-        firstTime = moment(firstTime, "H:mm A");
-
-        var nextTrain = moment.duration(currentTime.diff(firstTime));
-        nextTrain = Math.round(nextTrain.asMinutes());
-        nextTrain = frequency - (nextTrain % frequency);
-        var nextArrival = moment(currentTime + nextTrain);
-        // nextArrival = moment(nextArrival, "H:mm A").format();
-        console.log(currentTime);
-        console.log(firstTime);
-
-        if (currentTime < firstTime) {
-            var nextTrain = moment.duration(firstTime.diff(currentTime));
-            nextTrain = Math.round(nextTrain.asMinutes());
-            nextTrain = frequency - (nextTrain % frequency);
-            var nextTime = $("<td>").text(firstTime);
-            var minsAway = $("<td>").text((nextTrain) + " Minutes Away");
-
-        }
-        else {
-            var nextTime = $("<td>").text(nextArrival);
-            var minsAway = $("<td>").text((nextTrain) + " Minutes Away");
-        }
+        var minutesAway = currentTime.diff(moment.unix(firstTime), "minutes");
+        minutesAway = minutesAway % frequency;
+        minutesAway = frequency - minutesAway;
+        nextArrival = currentTime.add(minutesAway, "m").format("h:mm A");
+        var nextTime = $("<td>").text(nextArrival);
+        var minsAway = $("<td>").text((minutesAway) + " Minutes Away");
 
         newRow.append(shipName);
         newRow.append(shipDest);
